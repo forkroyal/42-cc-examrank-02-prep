5deef61c4161a357ac95218e3a7c9597528c1233
forkroyal <gfabits12@gmail.com>
2025-10-31 14:02:04 +0100

    '600'

---
+++ Level1/putstr/putstr.c
@@ -0,0 +1,16 @@
+#include <unistd.h>
+
+void	ft_putstr(char *str);
+
+void	ft_putstr(char *str)
+{
+	int i = 0;
+	
+	while (str[i])
+		write(1, &str[i++], 1);
+}
+
+int main(void)
+{
+	ft_putstr("hello 42");
+}
