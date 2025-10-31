9dfa81d7a3f4406c4a94e39e345c4c74cd1aa5c5
forkroyal <gfabits12@gmail.com>
2025-10-31 15:58:47 +0100

    '600'

---
+++ Level1/rot13/rot13.c
@@ -0,0 +1,50 @@
+#include <unistd.h>
+	 
+int main(int ac, char **av)
+{
+	if (ac != 2)
+	{
+		write(1, "\n", 1);
+		return 0;
+	}
+
+	int i = 0;
+	char *s = av[1];
+	
+	while (s[i])
+	{
+		char tow;
+		if (s[i] >= 'a' && s[i] <= 'z')
+		{
+			if (s[i] + 13 > 'z')
+			{
+				tow = s[i]-13;
+				write(1, &tow, 1);
+			}
+			else
+			{
+				tow = s[i]+13;
+				write(1, &tow, 1);
+			}
+			i++;
+		}
+		else if (s[i] >= 'A' && s[i] <= 'Z')
+		{
+
+			if (s[i] + 13 > 'Z')
+			{
+				tow = s[i]-13;
+				write(1, &tow, 1);
+			}
+			else
+			{
+				tow = s[i]+13;
+				write(1, &tow, 1);
+			}
+			i++;
+		}
+		else
+			write(1, &s[i++], 1);
+	}
+	write(1, "\n", 1);
+}
