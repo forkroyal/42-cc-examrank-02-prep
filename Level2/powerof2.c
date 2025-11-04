53916bdf91e494c248a24771a00b243a17e2c940
Fridolin Sitter <fsitter@c2r4p9.42vienna.com>
2025-11-04 10:49:50 +0100

    '600'

---
+++ Level2/powerof2.c
@@ -0,0 +1,19 @@
+int	    is_power_of_2(unsigned int n)
+{
+    if (n == 0)
+        return (0);
+    while (n != 1 && n % 2 == 0)
+	{
+		n /= 2;
+	}
+	if ( n == 1)
+		return 1;
+	else	
+		return 0;
+}
+
+
+int main()
+{
+	printf("%i\n", is_power_of_2(600));
+}
